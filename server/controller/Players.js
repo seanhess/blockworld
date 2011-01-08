4ab67de2aa46a61bb5a63d52248ce5c745b6482d
Rob (Robot) Middleton <rob@i.tv>
2011-01-08 00:20:02 -0700

    players server side

---
+++ server/controller/Players.js
@@ -0,0 +1,29 @@
+var sys = require("sys")
+var Player = require("./Player.js")
+
+var Players = []
+
+function generatePlayerId () {
+	return Math.floor(Math.random() * 10000000)
+}
+
+function getRandomPlayerId () {
+	var newPlayerId
+	while (Players[newPlayerId = generatePlayerId(newPlayerId)]) {
+	}
+	return newPlayerId
+}
+
+function getPlayerById (playerId) {
+	return Players[playerId]
+}
+
+exports.createPlayer = function (data) {
+	var newPlayerId = getRandomPlayerId()
+	Players[newPlayerId] = new Player()
+	return newPlayerId
+}
+
+exports.movePlayer = function (data) {
+	getPlayerById(data.playerId).move(data.x, data.y)
+}
