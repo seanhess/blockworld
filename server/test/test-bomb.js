44cce5a5cd905bf289d6bb2c8e871902060073eb
Sean Clark Hess <seanhess@gmail.com>
2011-05-18 15:07:27 -0700

    Fixed test

--- server/test/test-bomb.js
+++ server/test/test-bomb.js
@@ -14,45 +14,38 @@ var GameState = require('../model/GameState')
 var bombs = require('../controller/bomb.control')
 var walls = require('../controller/wall.control')
 
-//exports.addBomb = function (assert) {
-//
-//    helpers.setup(function(app, client) {
-//        helpers.client(function(secondClient) {
-//            client.send(new Player.MessageCreate({nickname:"nick"}))
-//
-//            helpers.gather(client, function(err, messages) {
-//                assert.ifError(err)
-//                Bomb.Delay = 200
-//                client.send(new Bomb.MessageCreate({x:2, y:2}))
-//
-//                helpers.gather(secondClient, function(err, messages) {
-//                    assert.ifError(err)
-//
-//                    var message = _(messages).detect(function(message) {
-//                        return message.type == Bomb.Type && message.action == Bomb.ActionCreate
-//                    })
-//
-//                    assert.ok(message, "didn't receive Bomb.ActionCreate")
-//                    assert.equal(message.data.x, 2, "Bomb x wasn't correct")
-//                    assert.equal(message.data.y, 2, "Bomb y wasn't correct")
-//
-//
-//                    helpers.gather(client, function(err, messages) {
-//                        assert.ifError(err)
-//                        
-//                        var message = _(messages).detect(function(message) {
-//                            return message.type == Bomb.Type && message.action == Bomb.ActionDetonate
-//                        })
-//                        
-//                        assert.ok(message, "Didn't receive Bomb.ActionDetonate")
-//                          
-//                        assert.finish()
-//                    })
-//                })
-//            })            
-//        })
-//    })                                        
-//}
+exports.addBomb = function (assert) {
+
+    helpers.setup(function(app, client) {
+        helpers.client(function(secondClient) {
+            client.send(new Player.MessageCreate({nickname:"nick"}))
+
+            helpers.gather(client, function(err, messages) {
+                assert.ifError(err)
+                Bomb.Delay = 300
+                client.send(new Bomb.MessageCreate({x:2, y:2}))
+
+                helpers.gather(secondClient, function(err, messages) {
+                    assert.ifError(err)
+
+                    var message = _(messages).detect(function(message) {
+                        return message.type == Bomb.Type && message.action == Bomb.ActionCreate
+                    })
+
+                    assert.ok(message, "didn't receive Bomb.ActionCreate")
+                    assert.equal(message.data.x, 2, "Bomb x wasn't correct")
+                    assert.equal(message.data.y, 2, "Bomb y wasn't correct")
+
+
+                    client.onMessage(function(message) {                          
+                        assert.ok(message, "Didn't receive Bomb.ActionDetonate")
+                        assert.finish()
+                    })
+                })
+            })            
+        })
+    })                                        
+}
 
 exports.hits = function(assert) {
     
@@ -93,19 +86,6 @@ exports.hits = function(assert) {
         if (hitWall && hitBomb)
             assert.finish()
     }
-    
-//    assert.ok(bomb.hitArea().indexOf(function(item) {
-//        return (item.x == 10 && item.y == 11)
-//    } > -1), "Bomb hit area incorrect")
-//    
-//    var hits = app.state().hitObjects(bomb.hitArea()) 
-//    console.log(hits)
-//    
-//    var index = hits.indexOf(function(item) {
-//        return (item.uid() == wall.uid())
-//    })
-    
-
     
 }
 
