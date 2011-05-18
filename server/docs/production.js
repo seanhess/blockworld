4aeef9e14b53e649ce8aef59bd30ff5a01c85f81
Sean Clark Hess <seanhess@gmail.com>
2011-05-17 18:06:25 -0700

    Npm update, cleanup

--- server/docs/production.js
+++
@@ -1,6 +0,0 @@
-https://github.com/lorenwest/node-monitor
-https://github.com/dulac/daemonchk.js/
-LAME - https://github.com/Slashed/daemon.node/
-LAME - https://github.com/ryantenney/node-fork
-LAME - https://github.com/Eyjafjallajokull/node-Daemon 
-https://github.com/theteam/node-daemon
